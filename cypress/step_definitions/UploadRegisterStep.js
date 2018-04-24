922e42d04d43c0fff58d64e5954b095eca1c0946
Bruno Macedo <brunom@ciandt.com>
2018-04-24 17:17:19 -0300

    Code improvment

--- cypress/step_definitions/UploadRegisterStep.js
+++ cypress/step_definitions/UploadRegisterStep.js
@@ -1,30 +1,6 @@
-const ManualRegister = require('../class/ManualRegister')
-let LIS_CODE = new ManualRegister().lisCode
+const Util = require('../class/Util')
+let lisCode = new Util().lisCode
 
 given('realizo um upload de XML para cadastrar uma ordem de serviço com o exame {string}', exam => {
-  cy.uploadValidOs(exam, LIS_CODE)
+  cy.uploadValidOs(exam, lisCode)
 })
-
-given('a ordem de serviço deve ser exibida na listagem de OS\'s', () => {
-  // not implemented
-})
-
-given('busco a ordem de serviço cadastrada', () => {
-  cy.searchForOsByLisCode(LIS_CODE)
-})
-
-given('vejo os exames da ordem de serviço', () => {
-  cy.get('.row-details__container').should('be.visible')
-})
-
-given('transmito a ordem de serviço', () => {
-  closeAllSnack()
-  cy.get('label[for="chkOrderSelectAllElement"]').click()
-  cy.get('#BtnOrdersSend').click()
-})
-
-function closeAllSnack () {
-  cy.get('.close-snack-container-btn').each($el => {
-    $el.click()
-  })
-}
